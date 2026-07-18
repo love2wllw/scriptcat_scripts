64afc31a123fb05369791fc33a4387cb16805c10
love2wllw <love2wllw@qq.com>
2026-07-19 07:27:35 +0800

    update

--- other/看料过滤广告栏.js
+++ other/看料过滤广告栏.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         看料过滤广告栏
-// @version      0.1.0
+// @version      0.1.1
 // @namespace    https://github.com/love2wllw/scriptcat_scripts/
 // @updateURL    https://raw.githubusercontent.com/love2wllw/scriptcat_scripts/refs/heads/main/other/看料过滤广告栏.js
 // @downloadURL  https://raw.githubusercontent.com/love2wllw/scriptcat_scripts/refs/heads/main/other/看料过滤广告栏.js
@@ -15,9 +15,10 @@
     'use strict';
 
     function removeAds() {
-        const predicate1 = (x) => x.querySelector("h2>div[class='wrap']>span[class='wraps']") !== undefined;
+        const predicate1 = (x) => x.querySelector("h2>div[class='wrap']>span[class='wraps']") != null;
         const predicate2 = (x) => [...x.querySelectorAll("div[class='post-card-info']>span")].some(x => x.innerText == "无分类");
         const articles = [...document.querySelectorAll("article")].filter(x => predicate1(x) && predicate2(x));
+        console.log(articles);
         for (let i = 0; i < articles.length; i++) {
             articles[i].remove();
         }
